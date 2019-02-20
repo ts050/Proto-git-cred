f6b95d22dd0d83a65a8767e586f9ffdd8baca421
Vivek Chowdary <34910461+ts050@users.noreply.github.com>
2019-02-20 15:06:07 +0530

    Update git-settings-helper.sh

--- git-settings-helper.sh
+++ git-settings-helper.sh
@@ -18,6 +18,7 @@ git config --global user.email $email;
 git config --global --unset credential.helper;
 # cmdkey commands are used in the bat files to remove existing credentials
 # TODO - Fiure out a way to store the credentials in a way where a new token is not created for every call
+# TODO - Figure out a way to handle special characters in username or passwords when the remote call are made
 ./git-cred-deletion.bat;
 git config --global credential.helper wincred;
 git pull https://$user:$password@${remote#"https://"} develop;
