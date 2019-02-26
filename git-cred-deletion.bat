d6ef1b5f9d24d34a9b49f0b07728dbff8c032ad1
ramuthe4nd <rammohan_gogula@secura.net>
2019-02-26 13:26:16 +0530

    Made changes to handle special characters in passwords and also handle redundant cmd keys

--- git-cred-deletion.bat
+++ git-cred-deletion.bat
@@ -1,3 +1,4 @@
 cmdkey /list
 cmdkey /delete:LegacyGeneric:target=git:https://github.com
 cmdkey /delete:LegacyGeneric:target=git:https://PersonalAccessToken@github.com
+cmdkey /delete:LegacyGeneric:target=git:https://%~1@github.com
--- git-settings-helper.sh
+++ git-settings-helper.sh
@@ -19,7 +19,9 @@ git config --global --unset credential.helper;
 # cmdkey commands are used in the bat files to remove existing credentials
 # TODO - Fiure out a way to store the credentials in a way where a new token is not created for every call
 # TODO - Figure out a way to handle special characters in username or passwords when the remote call are made
-./git-cred-deletion.bat;
+./git-cred-deletion.bat $user;
+
 git config --global credential.helper wincred;
-git pull https://$user:$password@${remote#"https://"} develop;
+cmd "/c cmdkey /generic:LegacyGeneric:target=git:https://github.com /user:$user /pass:$password"
+# git pull https://"\'$user\'":"\'$password\'"@${remote#"https://"} develop;
 echo "changed active git account to $user successfully";
